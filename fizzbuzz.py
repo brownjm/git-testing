85f7964d82ee95b7716543d7722dd3bc3f4f22b0
Jeffrey M. Brown <brown.jeffreym@gmail.com>
2021-03-03 11:55:28 -0600

    Added fizzybuzzy feature

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -2,12 +2,15 @@
 
 If number mod 3 print Fizz
 If number mod 5 print Buzz
+if number mod 7 print FizzyBuzzy
 If both print FizzBuzz
 """
 
 for i in range(100):
     if i % 15 == 0:
         print("{: 3d} FizzBuzz".format(i))
+    elif i % 7 == 0:
+        print("{: 3d} FizzyBuzzy".format(i))
     elif i % 3 == 0:
         print("{: 3d} Fizz".format(i))
     elif i % 5 == 0:
