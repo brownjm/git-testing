47d97f59b683e9d81513610a50f86541194b824a
Mandy Abernathy <mandyiabernathy@gmail.com>
2021-03-03 11:56:30 -0600

    added comments

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -6,11 +6,11 @@ If both print FizzBuzz
 """
 
 for i in range(100):
-    if i % 15 == 0:
+    if i % 15 == 0: # both 3 and 5 divide i
         print("{: 3d} FizzBuzz".format(i))
-    elif i % 3 == 0:
+    elif i % 3 == 0: # only 3 divides i
         print("{: 3d} Fizz".format(i))
-    elif i % 5 == 0:
+    elif i % 5 == 0: # only 5 divides i
         print("{: 3d} Buzz".format(i))
-    else:
+    else: # neither 3 nor 5 divides i
         pass
